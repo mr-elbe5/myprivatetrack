045be5ad97477b0fdcb56568556058a64cdfa7d2
mr-elbe5 <miro@elbe5.de>
2022-02-15 10:13:23 +0100

    Update AudioRecorder.swift

--- myprivatetrack/AVMedia/AudioRecorder.swift
+++ myprivatetrack/AVMedia/AudioRecorder.swift
@@ -16,24 +16,51 @@ class AudioRecorder: NSObject, AVCaptureAudioDataOutputSampleBufferDelegate{
     private var audioInput: AVAssetWriterInput!
     private var session: AVCaptureSession!
     private let sessionQueue = DispatchQueue(label: "audio session queue")
-    private var isSessionStarted = false
-    
     private var audioSettings: [String: Any]? = nil
     
     let audioBufferQueue = DispatchQueue(label: "audio buffer queue")
-
-    override init(){
-        super.init()
+    
+    func prepare() {
         AVCaptureDevice.askAudioAuthorization(){ result in
-            self.sessionQueue.async {
-                if self.prepareFileWriter(){
-                    self.prepareAudioDevice(with: self.sessionQueue)
+            let device: AVCaptureDevice = AVCaptureDevice.default(for: .audio)!
+            var audioDeviceInput: AVCaptureDeviceInput?
+            do {
+                audioDeviceInput = try AVCaptureDeviceInput(device: device)
+            } catch {
+                audioDeviceInput = nil
+            }
+            
+            let audioDataOutput = AVCaptureAudioDataOutput()
+            audioDataOutput.setSampleBufferDelegate(self, queue: self.sessionQueue)
+            
+            self.session = AVCaptureSession()
+            self.session.sessionPreset = .medium
+            self.session.usesApplicationAudioSession = true
+            self.session.automaticallyConfiguresApplicationAudioSession = false
+            
+            if self.session.canAddInput(audioDeviceInput!) {
+                self.session.addInput(audioDeviceInput!)
+            }
+            if self.session.canAddOutput(audioDataOutput) {
+                self.session.addOutput(audioDataOutput)
+            }
+            self.audioSettings = audioDataOutput.recommendedAudioSettingsForAssetWriter(writingTo: .m4v)
+            self.audioInput = AVAssetWriterInput(mediaType: .audio, outputSettings: self.audioSettings)
+            self.audioInput.expectsMediaDataInRealTime = true
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if let input = audioInput {
+            audioBufferQueue.async {
+                if input.isReadyForMoreMediaData {
+                    input.append(sampleBuffer)
                 }
             }
         }
     }
     
-    func prepareFileWriter() -> Bool{
+    private func prepareFileWriter() -> Bool{
         do{
             let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
             let fileUrl = paths[0].appendingPathComponent("output.m4a")
@@ -47,48 +74,15 @@ class AudioRecorder: NSObject, AVCaptureAudioDataOutputSampleBufferDelegate{
         return true
     }
     
-    func prepareAudioDevice(with queue: DispatchQueue) {
-        let device: AVCaptureDevice = AVCaptureDevice.default(for: .audio)!
-        var audioDeviceInput: AVCaptureDeviceInput?
-        do {
-            audioDeviceInput = try AVCaptureDeviceInput(device: device)
-        } catch {
-            audioDeviceInput = nil
-        }
-        
-        let audioDataOutput = AVCaptureAudioDataOutput()
-        audioDataOutput.setSampleBufferDelegate(self, queue: queue)
-        
-        session = AVCaptureSession()
-        session.sessionPreset = .medium
-        session.usesApplicationAudioSession = true
-        session.automaticallyConfiguresApplicationAudioSession = false
-        
-        if session.canAddInput(audioDeviceInput!) {
-            session.addInput(audioDeviceInput!)
-        }
-        if session.canAddOutput(audioDataOutput) {
-            session.addOutput(audioDataOutput)
-        }
-        audioSettings = audioDataOutput.recommendedAudioSettingsForAssetWriter(writingTo: .m4v)
-        audioInput = AVAssetWriterInput(mediaType: .audio, outputSettings: audioSettings)
-        audioInput.expectsMediaDataInRealTime = true
-        audioBufferQueue.async {
-            self.session.startRunning()
-        }
-        if assetWriter.canAdd(audioInput) {
-            assetWriter.add(audioInput)
-        }
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        if let input = audioInput {
+    func start() -> Bool{
+        if let session = session, prepareFileWriter(), assetWriter.canAdd(audioInput) {
             audioBufferQueue.async {
-                if input.isReadyForMoreMediaData {
-                    input.append(sampleBuffer)
-                }
+                session.startRunning()
             }
+            assetWriter.add(audioInput)
+            return true
         }
+        return false
     }
     
     func end(writing finished: @escaping () -> Void) {
